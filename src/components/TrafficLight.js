b5badba4e82df396d89cf1e153e31527a66380ee
trungnn <trungnn160697@gmail.com>
2021-03-21 23:27:18 +0900

    hot_fix

--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -1,27 +1,42 @@
 import './TrafficLight.css'
 import classNames from 'classnames'
+import { useEffect, useState } from 'react'
 
 const RED = 0
 const ORANGE = 1
 const GREEN = 2
 
-var currentColor = GREEN
-
-function getNextColor(color){
-  switch(color){
-    case RED:
-      return ORANGE
-    case ORANGE:
-      return GREEN
-    default:
-      return RED
-  }
-}
-
 function TrafficLight(props) { 
-  console.log(currentColor);
+  const [currentColor, setCurrentColor] = useState(GREEN);
+  const [flag, setFlag] = useState(false);
+  const getNextColor = () => {
+    switch(currentColor){
+      case RED:
+        setCurrentColor(ORANGE);
+        break;
+      case ORANGE:
+        setCurrentColor(GREEN);
+        break;
+      default:
+        setCurrentColor(RED);
+        break;
+    }
+  }
+  useEffect(() => {
+    getNextColor();
+  }, [flag]);
+
+
+  setTimeout(() => {
+    setFlag(!flag);
+  }, 1000);
+  
   return (
+    <div>      
+    <button>click</button>
+
     <div className="TrafficLight">
+      
       <div className={classNames('buld', 'red', {
         active: currentColor === RED
       })}></div>
@@ -32,11 +47,8 @@ function TrafficLight(props) {
         active: currentColor === GREEN
       })}></div>
     </div>
+    </div>
   )
 }
-
-setInterval(() => {
-  currentColor = getNextColor(currentColor)
-}, 1000)
 
 export default TrafficLight;
